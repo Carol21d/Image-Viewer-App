9fd6257b8bade622248bd4755b7ed7596b98c28e
blake S <design.harls@icloud.com>
2023-06-24 17:02:22 +0200

    corrigiendo errores de las flechas, app funciona correcta

--- image.py
+++ image.py
@@ -30,15 +30,29 @@ def forward(image_number):
     button_forward = Button(
         root, text=">>", command=lambda: forward(image_number+1))
     button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+
+    if image_number == 5:
+        button_forward = Button(root, text=">>", state=DISABLED)
+
     my_label.grid(row=0, column=0, columnspan=3)
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
 
 
-def back():
+def back(image_number):
     global my_label
     global button_back
     global button_forward
+
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_number-1])
+    button_forward = Button(
+        root, text=">>", command=lambda: forward(image_number+1))
+    button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_back.grid(row=1, column=0)
+    button_forward.grid(row=1, column=2)
 
 
 button_back = Button(root, text="<<", command=back)
