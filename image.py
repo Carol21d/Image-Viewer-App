1b2cde9e57dd1796cce39e8b2c8c0eb527810c4d
blake S <design.harls@icloud.com>
2023-06-24 16:45:11 +0200

    la app cambia de fotos cuando haces click en las flechas

--- image.py
+++ image.py
@@ -20,17 +20,30 @@ my_label = Label(image=my_img)
 my_label.grid(row=0, column=0, columnspan=3)
 
 
-def forward():
-    return
+def forward(image_number):
+    global my_label
+    global button_back
+    global button_forward
+
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_number-1])
+    button_forward = Button(
+        root, text=">>", command=lambda: forward(image_number+1))
+    button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_back.grid(row=1, column=0)
+    button_forward.grid(row=1, column=2)
 
 
 def back():
-    return
+    global my_label
+    global button_back
+    global button_forward
 
 
 button_back = Button(root, text="<<", command=back)
 button_exit = Button(root, text="EXIT PROGRAM", command=root.quit)
-button_forward = Button(root, text=">>", command=forward)
+button_forward = Button(root, text=">>", command=lambda: forward(2))
 
 
 button_back.grid(row=1, column=0)
