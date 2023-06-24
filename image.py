25ab01cf2ad1da0d3a21ce16bf51d2f6e5535735
blake S <design.harls@icloud.com>
2023-06-24 16:06:06 +0200

    añadiendo ruta de carpeta y las imagenes

---
+++ image.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+
+root = Tk()
+root.title = "Image Viewer App"
+root.iconbitmap(
+    "C:/Users/d3sig/OneDrive/Desktop/Projects-Python/Image-Viewer-App")
