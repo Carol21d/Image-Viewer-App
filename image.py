241b4e323eaae3780de0a03905c8d24c2a003030
blake S <design.harls@icloud.com>
2023-06-29 18:13:20 +0200

    las imagenes pertenecen al numero correcto al darle click a la flecha

--- image.py
+++ image.py
@@ -37,8 +37,8 @@ def forward(image_number):
     my_label.grid(row=0, column=0, columnspan=3)
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
-    status = Label(root, text=" " + str(image_number) +
-                   " " + str(len(image_list)))
+    status = Label(root, text="Image " + str(image_number) +
+                   " of  " + str(len(image_list)))
     status.grid(row=2, columnspan=3, sticky=W+E)
 
 
